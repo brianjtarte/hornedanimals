b66281727fa0a6ae628128bf9b83b5579d0eb751
brianjtarte <brianjtarte@gmail.com>
2021-12-11 14:47:24 -0800

    new branch

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Horned Animals</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Horned Animals</p>
+        <HornedBeasts title={'A Horned Beast'} description={'a unified unicorn'}/>
+      
+      </>
+    )
+  }
+};
+
+export default Main;

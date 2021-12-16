520ae5efe08682c1947370df7317b9c32c80676d
brianjtarte <brianjtarte@gmail.com>
2021-12-15 21:28:56 -0800

    added grid array to lab and attempted voting

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,17 +1,41 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card'
 
 class HornedBeasts extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      "beastVote": 0
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      beastVote: this.beastVote + 1
+    })
+  };
+  
   render() {
-    
+
     return (
       <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imgUrl} alt='horned cow' title='a horned cow'/>
-        <p>{this.props.description}</p>
+        <Card style={{ width: '18rem' }} onClick={this.handleClick}>
+          <Card.Img variant="top" src={this.props.beastObj.image_url} alt={this.props.beastObj.keyword} />
+          <Card.Body>
+            <Card.Title>{this.props.beastObj.title}</Card.Title>
+            <Card.Text>
+            {this.props.beastObj.description}
+            {this.state.beastVote}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+
+
       </>
     )
   }
 };
+
 
 export default HornedBeasts;
--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
 import './App'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import HornedBeasts from './HornedBeasts';
 
 class Main extends React.Component {
   render() {
+
     return (
       <>
         <p>Horned Animals</p>
-        <HornedBeasts title={'Horned Beast'}
-         imgUrl={'https://images.pexels.com/photos/460223/pexels-photo-460223.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}
-         description={'A hellacious horned animal'}/>
-          <HornedBeasts title={'Horned Beast'}
-         imgUrl={'https://images.pexels.com/photos/9906453/pexels-photo-9906453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}
-         description={'Scurry Furry Animal'}/>
+        <Container className="main">
+          <Row xs={1} md={2} lg={4} className="g-4">
+            {this.props.beastImg.map(beast => (
+              <Col>
+                <HornedBeasts beastObj={beast} />
+              </Col>
+              ))}
+          </Row>
+        </Container>
 
-      
       </>
     )
   }
